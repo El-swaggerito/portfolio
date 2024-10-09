b08fedc01c5833a441756dcb8dd6fcc54afb007a
Muhammad Zayyad Mukhtar <zayyadmukhtar@gmail.com>
2024-10-09 21:31:28 +0100

    final commit

---
+++ src/constants/index.js
@@ -0,0 +1,240 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "About",
+    href: "#about",
+  },
+  {
+    id: 3,
+    name: "Projects",
+    href: "#projects",
+  },
+  {
+    id: 4,
+    name: "Experience",
+    href: "#work",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    href: "#contact",
+  },
+];
+
+export const myProjects = [
+  {
+    title: "Chat App - Real-Time Collaboration",
+    desc: " It enables users to create accounts, send and receive messages instantly, and join chat rooms or groups. The app uses MongoDB for database management, ensuring efficient storage and retrieval of user data and chat history. Express.js and Node.js provide the backend server functionality, managing user authentication, socket connections for real-time updates, and RESTful APIs. React powers the user interface, offering a dynamic and responsive experience with features like notifications, user status indicators, and chatroom switching.",
+    subdesc:
+      "Built with the MERN stack (MongoDB, Express.js, React.js, Node.js) along with JWT authentication and Socket.io for real-time communication can offer a robust and dynamic platform",
+    href: "https://chat-app-h995.onrender.com/",
+    git: "https://github.com/El-swaggerito/chat-app",
+    texture: "/textures/project/project2.mp4",
+    logo: "/assets/chat.png",
+    logoStyle: {
+      backgroundColor: "#13202F",
+      border: "0.2px solid #17293E",
+      boxShadow: "0px 0px 60px 0px #2F6DB54D",
+    },
+    spotlight: "/assets/spotlight2.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: "IPhone 15 Site clone",
+    desc: "It allows visitors to explore the device from various angles, zoom in to view intricate details, and interact with the model using intuitive controls. GSAP enhances the experience with high-performance animations, creating smooth transitions, scroll-triggered effects, and dynamic scene changes. React provides a responsive and modular frontend, enabling seamless integration of interactive elements, such as feature highlights, color customization, and real-time updates, delivering an engaging and visually appealing experience.",
+    subdesc:
+      "Built using Three.js for displaying 3D model, GSAP for high-performance animations on the web and React",
+    href: "https://iphone15pro-clone-coral.vercel.app/",
+    git: "https://github.com/El-swaggerito/iphone15-clone",
+    texture: "/textures/project/project3.mp4",
+    logo: "/assets/apple.png",
+    logoStyle: {
+      backgroundColor: "#60f5a1",
+      background:
+        "linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)",
+      border: "0.2px solid rgba(208, 213, 221, 1)",
+      boxShadow: "0px 0px 60px 0px rgba(35, 131, 96, 0.3)",
+    },
+    spotlight: "/assets/spotlight3.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: "Instagram Clone App",
+    desc: "ReactJS powers the frontend, delivering a dynamic and responsive user interface where users can browse feeds, upload photos, and interact with posts. Chakra UI provides a sleek, accessible design system for consistent styling, enabling a clean and visually appealing layout across devices. Firebase handles the backend functionalities, including real-time data synchronization, user authentication, and secure storage for media files and user data. This combination ensures a smooth and scalable user experience, with features like live notifications, user profiles, and search functionality.",
+    subdesc:
+      "Built using ReactJS for the front end, Chakra UI for styling, and Firebase for the backend",
+    href: "https://cloned-ig.vercel.app/",
+    git: "https://github.com/El-swaggerito/Instagram-clone",
+    texture: "/textures/project/project4.mp4",
+    logo: "/assets/instagram.png",
+    logoStyle: {
+      backgroundColor: "#0E1F38",
+      border: "0.2px solid #0E2D58",
+      boxShadow: "0px 0px 60px 0px #2F67B64D",
+    },
+    spotlight: "/assets/spotlight4.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: "Snapcode - Snapchat Clone App ",
+    desc: "The app leverages Next.js for server-side rendering and dynamic routing, providing fast page loads and a smooth navigation experience. TypeScript ensures robust type-checking and code quality across the project, resulting in a more reliable and maintainable codebase. MongoDB serves as the database, storing user data, media, and chat history efficiently. Tailwind CSS powers the styling, enabling a highly customizable, responsive, and visually appealing interface. Snapcode supports features like photo and video sharing, real-time messaging, disappearing snaps, and story updates, mimicking the core functionalities of Snapchat with a modern twist.",
+    subdesc:
+      "Snapcode is a Snapchat clone built with a modern tech stack that includes Next.js, TypeScript, MongoDB, and Tailwind CSS, delivering a seamless, real-time social media experience.",
+    href: "https://snapcode-muhammad-zayyad-mukhtars-projects.vercel.app/",
+    git: "https://github.com/El-swaggerito/Snapcode",
+    texture: "/textures/project/project5.mp4",
+    logo: "/assets/snapchat.png",
+    logoStyle: {
+      backgroundColor: "#1C1A43",
+      border: "0.2px solid #252262",
+      boxShadow: "0px 0px 60px 0px #635BFF4D",
+    },
+    spotlight: "/assets/spotlight5.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall
+      ? [4, -5, 0]
+      : isMobile
+      ? [5, -5, 0]
+      : isTablet
+      ? [5, -5, 0]
+      : [9, -5.5, 0],
+    reactLogoPosition: isSmall
+      ? [3, 4, 0]
+      : isMobile
+      ? [5, 4, 0]
+      : isTablet
+      ? [5, 4, 0]
+      : [12, 3, 0],
+    ringPosition: isSmall
+      ? [-5, 7, 0]
+      : isMobile
+      ? [-10, 10, 0]
+      : isTablet
+      ? [-12, 10, 0]
+      : [-24, 10, 0],
+    targetPosition: isSmall
+      ? [-5, -10, -10]
+      : isMobile
+      ? [-9, -10, -10]
+      : isTablet
+      ? [-11, -7, -10]
+      : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: "Kwara State University",
+    pos: "Student",
+    duration: "September 2020 - August 2024",
+    title: "Bachelor's degree in computer science (Second Class Upper)",
+    icon: "/assets/framer.svg",
+    animation: "victory",
+  },
+  {
+    id: 2,
+    name: "Ecalpemos Technologies",
+    pos: "Undergraduate Intern",
+    duration: "July 2023 - December 2023",
+    title:
+      "Collaborated with team members to design and implement software solutions.",
+    animation: "clapping",
+  },
+];
